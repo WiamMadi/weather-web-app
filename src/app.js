1144ab65590c5f651fbb9ffaaea5168e8e9b9336
WiamMadi <wiammadi@icloud.com>
2023-03-24 16:57:18 -0600

    Created a basic page layout, setup the express server in app.js, created a public folder, created a css file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
-import getLocation from "./services/location.service.js";
-import {
-  getWeatherByCity,
-  getWeatherByLL,
-} from "./services/weather.service.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
 
-getLocation().then((res) => {
-  //   getWeatherByLL(res["latitude"], res["longitude"]).then((weather) => {
-  //     console.log(weather);
-  //   });
-  //   getWeatherByCity(res["city"]).then(weather => {
-  //     console.log(weather);
-  //   });
+import getLocation from "./services/location.service.js";
+
+/* Getting current directory */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/* Express app settings */
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+/* Express routes */
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+/* Start server and listen on provided port */
+app.listen(port, () => {
+  console.log(`Server started, running on port ${port}`);
 });
